1a4d09f82452fd4145c9f102d01a730ac083fd0f
Nikita Shulga <nikita.shulga@gmail.com>
2024-02-06 10:43:11 -0800

    Run key, query and value projections in one batch
    Stolen from https://github.com/pytorch-labs/gpt-fast/blob/eb1789be0bdb7a7b75291f0839532ce1931305a2/model.py#L144t
    
    At least for fp32-eager doesn't make that much of a difference

--- tinystories-llm.py
+++ tinystories-llm.py
@@ -89,20 +89,27 @@ class Attention(nn.Module):
         model_parallel_size = 1
         self.n_local_heads = args.n_heads // model_parallel_size
         self.head_dim = args.dim // args.n_heads
-        self.wq = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
-        self.wk = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
-        self.wv = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
+        # key, query, value projections for all heads, but in a batch
+        self.wqkv = nn.Linear(args.dim, 3 * args.n_heads * self.head_dim, bias=False)
         self.wo = nn.Linear(args.n_heads * self.head_dim, args.dim, bias=False)
         self.attn_dropout = nn.Dropout(args.dropout)
         self.resid_dropout = nn.Dropout(args.dropout)
         self.dropout = args.dropout
         self.flash = use_flash
+        self._register_load_state_dict_pre_hook(self.load_hook)
 
         # use flash attention or a manual implementation?
         if not self.flash:
             mask = torch.full((1, 1, args.max_seq_len, args.max_seq_len), float("-inf"))
             mask = torch.triu(mask, diagonal=1)
             self.register_buffer("mask", mask)
+
+    def load_hook(self, state_dict, prefix, *args):
+        if prefix + "wq.weight" in state_dict:
+            wq = state_dict.pop(prefix + "wq.weight")
+            wk = state_dict.pop(prefix + "wk.weight")
+            wv = state_dict.pop(prefix + "wv.weight")
+            state_dict[prefix + "wqkv.weight"] = torch.cat([wq, wk, wv])
 
     def forward(
         self,
@@ -113,7 +120,8 @@ class Attention(nn.Module):
         bsz, seqlen, _ = x.shape
 
         # QKV
-        xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
+        kv_size = self.n_local_heads * self.head_dim
+        xq, xk, xv = self.wqkv(x).split(kv_size, dim=-1)
         xq = xq.view(bsz, seqlen, self.n_local_heads, self.head_dim)
         xk = xk.view(bsz, seqlen, self.n_local_heads, self.head_dim)
         xv = xv.view(bsz, seqlen, self.n_local_heads, self.head_dim)
