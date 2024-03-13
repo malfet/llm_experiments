35fbc7cfceaa528a5ce4178b0f906f1045d9709e
Nikita Shulga <nikita.shulga@gmail.com>
2024-03-13 08:18:59 -0700

    Add simple `torch.rand` benchmark
    Though results are unsuprising: on 2.2 float16 is much slower and then rest

---
+++ benchmarks/benchmark_torch_rand.py
@@ -0,0 +1,37 @@
+# Benchmark torch.rand performance on varios platforms/dtypes
+# Against torch-2.2.0 for 4096x4096
+# |                  | float32 | float16  | bfloat16 |
+# | Apple M1         |         |          |          |
+# | Apple M2 Pro     | 50 msec | 137 msec | 50 msec  |
+# | AWS Tg4          |         |          |          |
+# | Xeon 8275CL@3Ghz |         |          |          |
+# | Xeon @2.2Ghz     |         |          |          |
+
+from timeit import default_timer
+
+import torch
+from torch.utils.benchmark import Measurement, Timer
+
+
+def bench_rand(
+    m,
+    n,
+    dtype=torch.float32,
+    device: str = "cpu",
+    trans_a: bool = False,
+    trans_b: bool = False,
+) -> Measurement:
+    t = Timer(
+        stmt=f"torch.rand(({m}, {n}), dtype={dtype}, device='{device}')", setup="", language="python", timer=default_timer
+    )
+    return t.blocked_autorange()
+
+
+if __name__ == "__main__":
+    torch.set_num_threads(1)
+    m, n = 4096, 4096
+    device = "cpu"
+    for dtype in [torch.float32, torch.float16, torch.bfloat16]:
+        rc = bench_rand(m, n, dtype, device=device)
+        use_msec = rc.mean > 1e-4
+        print(f"rand {str(dtype):>14} {rc.mean*(1e3 if use_msec else 1e6):>7.2f} {'m' if use_msec else 'u'}sec")
