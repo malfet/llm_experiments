27dff757671b9fad912c36655725f2c98fb42e33
Nikita Shulga <nikita.shulga@gmail.com>
2024-10-03 12:37:47 -0700

    And CUDA

--- benchmarks/benchmark_torch_trig.py
+++ benchmarks/benchmark_torch_trig.py
@@ -45,6 +45,15 @@ if __name__ == "__main__":
         uname = "msec" if use_msec else "usec"
         print(f"torch.sin+torch.cos({device}) {str(dtype):>14} {eager_t.mean*multiplier:>7.2f} {uname} {comp_t.mean*multiplier:>7.2f} {uname} {eager_t.mean/comp_t.mean:>7.2f}")
 
+    if torch.cuda.is_available():
+        device = "cuda"
+        for dtype in [torch.float32, torch.float16, torch.bfloat16]:
+            eager_t = bench_unary(m, n, f, dtype, device=device)
+            comp_t = bench_unary(m, n, f_c, dtype, device=device)
+            use_msec = eager_t.mean > 1e-4 or comp_t.mean > 1e-4
+            multiplier = 1e3 if use_msec else 1e6
+            uname = "msec" if use_msec else "usec"
+            print(f"torch.sin+torch.cos({device}) {str(dtype):>14} {eager_t.mean*multiplier:>7.2f} {uname} {comp_t.mean*multiplier:>7.2f} {uname} {eager_t.mean/comp_t.mean:>7.2f}")
     if torch.backends.mps.is_available():
         device = "mps"
         for dtype in [torch.float32, torch.float16, torch.bfloat16]:
