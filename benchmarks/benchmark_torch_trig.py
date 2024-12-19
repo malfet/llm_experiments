57e9384632654aa960170f10942e461234959a31
Nikita Shulga <nikita.shulga@gmail.com>
2024-12-19 10:15:40 -0800

    Add fake mps_compile

--- benchmarks/benchmark_torch_trig.py
+++ benchmarks/benchmark_torch_trig.py
@@ -29,6 +29,14 @@ def bench_unary(
     return t.blocked_autorange()
 
 
+mps_ext = None
+def mps_compile(f):
+    global mps_ext
+    if mps_ext is None:
+        mps_ext = torch.utils.cpp_extension.load(name="mps_ext", sources=["mps_sum_sincos.mm"])
+    return mps_ext.mps_sum_sincos
+
+
 if __name__ == "__main__":
     def f(x):
         return torch.sin(x) + torch.cos(x)
@@ -56,11 +64,10 @@ if __name__ == "__main__":
             uname = "msec" if use_msec else "usec"
             print(f"torch.sin+torch.cos({device}) {str(dtype):>14} {eager_t.mean*multiplier:>7.2f} {uname} {comp_t.mean*multiplier:>7.2f} {uname} {eager_t.mean/comp_t.mean:>7.2f}")
     if torch.backends.mps.is_available():
-        ext = torch.utils.cpp_extension.load(name="mps_ext", sources=["mps_sum_sincos.mm"])
         device = "mps"
         for dtype in [torch.float32, torch.float16, torch.bfloat16]:
             eager_t = bench_unary(m, n, f, dtype, device=device)
-            comp_t = bench_unary(m, n, ext.mps_sum_sincos, dtype, device=device)
+            comp_t = bench_unary(m, n, mps_compile(f), dtype, device=device)
             use_msec = eager_t.mean > 1e-4
             multiplier = 1e3 if use_msec else 1e6
             uname = "msec" if use_msec else "usec"
